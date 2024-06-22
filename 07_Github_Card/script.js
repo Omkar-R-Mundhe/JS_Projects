ca10bdc8c95675817f25bbc4c3ec256a58b9a6c9
Omkar-R-Mundhe <148869427+Omkar-R-Mundhe@users.noreply.github.com>
2024-06-22 11:06:10 +0530

    code changed

--- 07_Github_Card/script.js
+++ 07_Github_Card/script.js
@@ -2,22 +2,22 @@
 
 let avatar;
 const requestURL = "https://api.github.com/users/Omkar-R-Mundhe";
-const xhr = new XMLHttpRequest();
+const xhr = new XMLHttpRequest();//This type of request is asynchronus in nature
+//thus the requst goes on in backgroudn and onreadystatechange is used to handle the response
 xhr.open("GET", requestURL);
+
+xhr.send();
+
 xhr.onreadystatechange = function () {
   console.log(xhr.readyState);
   if (xhr.readyState == 4) {
     const resp = JSON.parse(this.responseText);
-    console.log(resp)
+    console.log(resp);
 
     document.querySelector(".card-img-top").src = resp.avatar_url;
     document.querySelector(".card-text").innerHTML = resp.bio;
     document.querySelector(".card-title").innerHTML = resp.name;
 
-
     document.querySelector(".btn-primary").href = resp.html_url;
-    
   }
 };
-
-xhr.send();
